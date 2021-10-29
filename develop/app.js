20a5812715034cf42d9f91d11a1da0de74892526
pavelsjo <pavels.jo@gmail.com>
2021-10-28 23:48:08 -0300

    Se agrega la interfaz para la creacion de cuentas

--- develop/app.js
+++ develop/app.js
@@ -1,3 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
-    M.AutoInit();
+document.addEventListener('DOMContentLoaded', () => {
+  
+  // materialize setup
+  M.AutoInit();
+
+  // dom querys
+  const buttonCreate = document.querySelector('#create-wallet');
+
+  // listeners
+  buttonCreate.addEventListener('click', e => {
+
+    console.log('click')
+    
   });
+
+});
