1026269827290b1ecd561856d668a0a6b9105897
pavelsjo <pavels.jo@gmail.com>
2021-10-29 00:02:17 -0300

    Funcionalidad de autocompletado

--- develop/app.js
+++ develop/app.js
@@ -2,6 +2,15 @@ document.addEventListener('DOMContentLoaded', () => {
   
   // materialize setup
   M.AutoInit();
+  var elems = document.querySelectorAll('.autocomplete');
+  var instances = M.Autocomplete.init(elems, {
+    data:{
+      "Meli": './icons/mercadopago.png',
+      "Brubank": './icons/brubank.png',
+      "Galicia": './icons/galicia.png',
+      "Uala": './icons/uala.jpg'
+    }
+  });
 
   // dom querys
   const buttonCreate = document.querySelector('#create-wallet');
